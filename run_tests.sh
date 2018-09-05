70ffb708285b52e7af907e855e5f688e50092459
Nigel Small <nigel@neo4j.com>
2018-09-05 15:14:05 +0100

    Few more tests

--- run_tests.sh
+++ run_tests.sh
@@ -4,17 +4,28 @@ ARGS=$*
 VERSIONS="3.5.0-alpha07 3.4.7 3.3.6 3.2.12"
 
 coverage erase
-coverage run -a -m pytest -v ${ARGS} test/unit test/stub #test/performance
+
+echo "Running unit tests"
+coverage run -a -m pytest -v ${ARGS} test/unit
 STATUS="$?"
 if [ ${STATUS} -ne 0 ]
 then
     exit ${STATUS}
 fi
+
+echo "Running stub tests"
+coverage run -a -m pytest -v ${ARGS} test/stub
+STATUS="$?"
+if [ ${STATUS} -ne 0 ]
+then
+    exit ${STATUS}
+fi
+
 if [ -z "${NEO4J_SERVER_PACKAGE}" ]
 then
     for VERSION in ${VERSIONS}
     do
-        echo "Integration testing against Neo4j ${VERSION}"
+        echo "Running integration tests against Neo4j ${VERSION}"
         NEO4J_SERVER_PACKAGE="http://dist.neo4j.org/neo4j-enterprise-${VERSION}-unix.tar.gz" coverage run -a -m pytest -v ${ARGS} test/integration
         STATUS="$?"
         if [ ${STATUS} -ne 0 ]
@@ -23,7 +34,7 @@ then
         fi
     done
 else
-    echo "Integration testing against Neo4j at ${NEO4J_SERVER_PACKAGE}"
+    echo "Running integration tests against Neo4j at ${NEO4J_SERVER_PACKAGE}"
     coverage run -a -m pytest -v ${ARGS} test/integration
     STATUS="$?"
     if [ ${STATUS} -ne 0 ]
@@ -31,4 +42,5 @@ else
         exit ${STATUS}
     fi
 fi
+
 coverage report
---
+++ test/unit/test_versioning.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+# Copyright (c) 2002-2018 "Neo4j,"
+# Neo4j Sweden AB [http://neo4j.com]
+#
+# This file is part of Neo4j.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from unittest import TestCase
+
+from neobolt.versioning import Version
+
+
+class VersionTestCase(TestCase):
+
+    def test_major_minor(self):
+        v = Version.parse("1.2")
+        self.assertEqual(v, ((1, 2),))
+
+    def test_major_minor_patch(self):
+        v = Version.parse("1.2.3")
+        self.assertEqual(v, ((1, 2, 3),))
+
+    def test_alpha(self):
+        v = Version.parse("1.2.3-alpha01")
+        self.assertEqual(v, ((1, 2, 3), ("alpha",), (1, )))
+
+    def test_snapshot(self):
+        v = Version.parse("1.2-snapshot")
+        self.assertEqual(v, ((1, 2), ("snapshot",)))
+
+    def test_double_dash(self):
+        v = Version.parse("1.2--3")
+        self.assertEqual(v, ((1, 2), (0,), (3,)))
+
+    def test_double_dot(self):
+        v = Version.parse("1.2..3")
+        self.assertEqual(v, ((1, 2, 0, 3),))
